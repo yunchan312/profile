244f0c816ff3dc1a2df92353ff78dff385770109
yunchan123 <phenomenal312@hufs.ac.kr>
2023-10-05 21:35:48 +0900

    modals starts

---
+++ src/Components/Modal.tsx
@@ -0,0 +1,3 @@
+export default function ProjectLists() {
+  return <div></div>;
+}
--- src/Routes/Studies.tsx
+++ src/Routes/Studies.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import TechBox from "../Components/TechBoxContainer";
+import ProjectLists from "Components/Modal";
 
 const Wrapper = styled.div`
   background-color: ${(props) => props.theme.orange.light};
@@ -11,12 +12,70 @@ const Title = styled.div`
   font-size: 60px;
   padding: 30px;
 `;
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin-left: auto;
+  margin-right: auto;
+  width: 90%;
+  height: 70vh;
+  overflow: hidden;
+  border-radius: 30px;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.7);
+  margin-top: 15px;
+`;
+const Box = styled.div<{ bgColor?: string }>`
+  background-color: ${(props) => props.theme.red};
+`;
+const STitle = styled.div`
+  font-size: 30px;
+  padding: 30px;
+  border-bottom: 5px dashed ${(props) => props.theme.white};
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 5px;
+`;
+const Projects = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 85%;
+  margin-left: auto;
+  margin-right: auto;
+  :hover {
+    background-color: rgba(0, 0, 0, 0.5);
+    cursor: pointer;
+  }
+`;
+const Project = styled.div`
+  font-size: 20px;
+  margin-bottom: 10px;
+  padding: 5px;
+`;
 
 export default function Studies() {
   return (
     <>
       <Wrapper>
-        <Title>🖥️What I use?</Title>
+        <Title>👨‍💻Projects</Title>
+        <Container>
+          <Box>
+            <STitle>🧚Solo</STitle>
+            <Projects>
+              <Project>Numbers!</Project>
+              <Project>Netflix_Cloning</Project>
+              <Project>NextJs_BestSeller</Project>
+              <Project>NextJs_CarrotMarket</Project>
+              <Project>Profile</Project>
+            </Projects>
+          </Box>
+          <Box>
+            <STitle>🤼Cooperation</STitle>
+          </Box>
+        </Container>
+      </Wrapper>
+      <Wrapper>
+        <Title>🖥️What I used?</Title>
         <TechBox />
       </Wrapper>
     </>
